8b92398e0d208618594905cca840ed0f5617580e
Rajveer Singh <singhrajveer4021@gmail.com>
2022-11-11 19:39:50 -0800

    adding do while loop that prompts user for a number that isn't a string and has a length greater than 3

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,13 @@ const courseList = [
     {code: "ACIT 1630", name: "Database Systems"}
 ]
 
+
+
+// Do while loop that asks the user for a 4 digit number and makes sure the entered number isn't and a string and has a length greater than 3 //
+
+let userInput;
+do{
+    userInput = prompt("Enter a 4 digit number:");
+    console.log(userInput);
+
+} while (isNaN(userInput) || userInput.length < 4);
