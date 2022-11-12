5c6f71ddf7e6db3718d7f0007e2dfc29fcda73de
Rajveer Singh <singhrajveer4021@gmail.com>
2022-11-11 18:25:33 -0800

    adding constant that contains course codes and names

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const courseList = [
+    {code: "ACIT 1420", name: "Introduction to Systems Administrator"},
+    {code: "ACIT 1620", name: "Fundamental Web Technologies"},
+    {code: "ACIT 1630", name: "Database Systems"}
+]
+
